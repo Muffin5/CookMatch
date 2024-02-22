0d93a82400124490acb5d2498f4137dd7c23752b
maria1804git <56427792+maria1804git@users.noreply.github.com>
2024-02-23 02:45:27 +0300

    Splash screen functional done

---
+++ CookMatch/app/src/main/java/muffin/experiments/cookmatch/SplashScreen.java
@@ -0,0 +1,30 @@
+package muffin.experiments.cookmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+
+
+    }
+
+}
