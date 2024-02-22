48962d438cc0c3fb303387ca2547f23d918d178a
maria1804git <56427792+maria1804git@users.noreply.github.com>
2024-02-23 01:28:48 +0300

    CookMatch

---
+++ CookMatch/app/src/main/java/muffin/experiments/cookmatch/MainActivity.java
@@ -0,0 +1,14 @@
+package muffin.experiments.cookmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
